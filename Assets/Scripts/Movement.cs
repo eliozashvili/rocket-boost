b051c8a8f17dd11941fe962a5da0e74191c2f0b3
eliozashvili <ilivhsazoile.g@gmail.com>
2026-06-05 00:06:35 +0400

    feat: initial commit, player movement implementation, cinemachine

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+   [SerializeField] private InputAction thrust;
+   [SerializeField] private InputAction rotation;
+   [SerializeField] private float thrustStrength;
+   [SerializeField] private float rotationStrength;
+
+   private Rigidbody _rb;
+
+   private bool _isThrusting;
+   private float _rotationInputValue;
+
+   private void Start()
+   {
+      _rb = GetComponent<Rigidbody>();
+   }
+
+   private void OnEnable()
+   {
+      thrust.Enable();
+      rotation.Enable();
+
+      thrust.performed += _ => _isThrusting = true;
+      thrust.canceled += _ => _isThrusting = false;
+
+      rotation.performed += context => _rotationInputValue = context.ReadValue<float>();
+      rotation.canceled += _ =>  _rotationInputValue = 0f;
+   }
+
+   private void OnDisable()
+   {
+      thrust.Disable();
+      rotation.Disable();
+   }
+
+   private void FixedUpdate()
+   {
+      if (!_isThrusting && Mathf.Approximately(_rotationInputValue, 0f)) return;
+
+      ProcessThrust();
+      ProcessRotation();
+   }
+
+   private void ProcessThrust()
+   {
+      if (!_isThrusting) return;
+
+      _rb.AddRelativeForce(thrustStrength * Time.fixedDeltaTime * Vector3.up);
+   }
+
+   private void ProcessRotation()
+   {
+      if (Mathf.Approximately(_rotationInputValue, 0f)) return;
+
+      var angleZ = -_rotationInputValue * rotationStrength * Time.fixedDeltaTime;
+
+      ApplyRotation(angleZ);
+   }
+
+   private void ApplyRotation(float angle)
+   {
+      var deltaRotate = Quaternion.Euler(new Vector3(0f, 0f, angle));
+
+      _rb.MoveRotation(_rb.rotation * deltaRotate);
+   }
+}
